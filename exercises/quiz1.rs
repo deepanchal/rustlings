f32ddad35b976ba8d3fcb3f07d902620a4279334
Deep Panchal <deep302001@gmail.com>
2023-03-11 10:52:59 -0600

    feat(sol): solve all quiz exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,19 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(n: u32) -> u32 {
+    let price = {
+        if n > 40 {
+            1
+        } else {
+            2
+        }
+    };
+    price * n
+}
 
 // Don't modify this function!
 #[test]
